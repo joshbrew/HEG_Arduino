fa5f24743bcada3ba75c4115958e73b57b92794f
Bubba Skrimp <crazykid4534@gmail.com>
2018-06-13 16:29:47 -0800

    edit

--- HEG_Arduino.ino
+++ HEG_Arduino.ino
@@ -2,25 +2,7 @@
 #include <Adafruit_ADS1015.h>
 
 /*
-  Analog input, analog output, serial output
-
-  Reads an analog input pin, maps the result to a range from 0 to 255 and uses
-  the result to set the pulse width modulation (PWM) of an output pin.
-  Also prints the results to the Serial Monitor.
-
-  The circuit:
-  - potentiometer connected to analog pin 0.
-    Center pin of the potentiometer goes to the analog pin.
-    side pins of the potentiometer go to +5V and ground
-  - LED connected from digital pin 9 to ground
-
-  created 29 Dec. 2008
-  modified 9 Apr 2012
-  by Tom Igoe
-
-  This example code is in the public domain.
-
-  http://www.arduino.cc/en/Tutorial/AnalogInOutSerial
+ Reads I2C input for the ADS1115, outputs power to LEDs.
 */
 
 // These constants won't change. They're used to give names to the pins used:
