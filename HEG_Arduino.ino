a4292bf0a00f24999853218123c068355144487d
Bubba Skrimp <crazykid4534@gmail.com>
2018-06-14 16:37:32 -0800

    updated with basic scoring system

--- HEG_Arduino.ino
+++ HEG_Arduino.ino
@@ -8,21 +8,38 @@
 // These constants won't change. They're used to give names to the pins used:
 const int analogInPin = A0;  // Analog input pin that the potentiometer is attached to
 const int analogOutPin = 9; // Analog output pin that the LED is attached to
-
+  // PUT IR IN D2, AND RED IN D3
 int16_t adc0; // Resulting 16 bit integer
 
 Adafruit_ADS1115 ads(0x48);
 float Voltage = 0.0;
-float range = 2^15 - 1;
-float gain = 2.048; // +/- V
+float range = 32767; // 16 bit ADC (15 bits of range minus one)
+float gain = 0.256           ; // +/- V
 float bits2mv = gain / range;
+
 bool first_led = false; // Bool to alternate LEDS
+bool badSignal = false;
+bool signalDetermined = false;
+
+int ticks0 = 0;
+int ticks1 = 0;
+int ticks2 = 0;
+
+long redValue = 0;
+long irValue = 0;
+float redavg = 0;
+float iravg = 0;
+float ratio = 0;
+float baseline = 0;
+float score = 0;
+
 unsigned long startMillis;
 unsigned long currentMillis;
 unsigned long ledMillis;
-const unsigned long ledrate = 500; // LED flash rate (ms)
-const unsigned long samplerate = 2; // ADC read rate (ms)
 
+//Make sure these divide without decimals remaining for best results
+const unsigned long ledRate = 500; // LED flash rate (ms)
+const unsigned long sampleRate = 2; // ADC read rate (ms). 
 
 void setup() {
   // initialize serial communications at 9600 bps:
@@ -31,7 +48,7 @@ void setup() {
   
   Serial.begin(9600);
   ads.begin();
-  ads.setGain(GAIN_TWO);
+  ads.setGain(GAIN_SIXTEEN);
 
   //ads.setGain(GAIN_TWOTHIRDS);  // 2/3x gain +/- 6.144V  1 bit = 3mV (default)
   //ads.setGain(GAIN_ONE);     // 1x gain   +/- 4.096V  1 bit = 2mV
@@ -48,25 +65,106 @@ void loop() {
 
   currentMillis = millis();
 
-  if(currentMillis - startMillis >= samplerate) {
+  if(currentMillis - startMillis >= sampleRate) {
 
     // read the analog in value:
     adc0 = ads.readADC_SingleEnded(0);
-    Voltage = (adc0 * bits2mv); 
+    Voltage = (adc0 * bits2mv); // FIX 
     
     // print the results to the Serial Monitor:
     Serial.print("ADC Value: ");
     Serial.println(adc0);
-    //Serial.print("\tVoltage: ");
+    //Serial.print("\tVoltage: "); 
     //Serial.println(Voltage,7);
+
+    if(adc0 >= 10000) { // The gain is high but anything over 30000 is most likely not a valid signal
+      Serial.print("\n Bad Read ");
+      badSignal = true;
+
+      //Temp: reset baseline on bad read
+      signalDetermined = false;
+      baseline = 0;
+
+      ticks0 = 0; // Reset counter
+      ticks1 = 0;
+      ticks2 = 0;
+      redValue = 0;
+      irValue = 0;
+    }
+    else {
+      badSignal = false;
+      if(signalDetermined == false){
+        ticks0++;
+        if(ticks0 > 100) { // Wait for 100 samples of good signal before getting baseline
+          // IR IN D2, RED IN D3
+          if(ticks0 < 300) { // Accumulate samples for baseline
+            if(first_led == true) { // RED
+              redValue += adc0;
+              ticks1++;
+
+            }
+            else { // IR
+              irValue += adc0;
+              ticks2++;
+            }
+          }
+          else {
+            signalDetermined = true;
+            redavg = redValue / ticks1;
+            iravg = irValue / ticks2;
+
+            baseline = redavg / iravg; // Set baseline ratio
+            ticks0 = 0; // Reset counters
+            ticks1 = 0;
+            ticks2 = 0;
+            redValue = 0; // Reset values
+            irValue = 0;
+
+            //Serial.print("\tBaseline R: ");
+            //Serial.println(baseline);
+          }
+        }
+      }
+      else {
+        ticks0++;
+        if(first_led == true) { // RED
+          redValue += adc0;
+          ticks1++;
+        }
+        else { // IR
+          irValue += adc0;
+          ticks2++;
+        }
+        if(ticks0 > 100) { // Accumulate 50 samples before taking reading
+          redavg = redValue / ticks1;
+          iravg = irValue / ticks2;
+          ratio = redavg / iravg;
+          score += ratio - baseline;
+          
+          //Serial.print("\tBaseline R: ");
+          //Serial.print(baseline);
+          //Serial.print("\tNow: ");
+          //Serial.print(ratio);
+          //Serial.print("\tScore: ");
+          //Serial.print(score);
+          //Serial.print("\n");
+
+          ticks0 = 0;
+          ticks1 = 0;
+          ticks2 = 0;
+        }
+      }
+    }
     
     // wait a minimum of 1.2 milliseconds before the next loop for the analog-to-digital
     // converter to settle after the last reading:
     startMillis = currentMillis;
     
   }
-  
-  if(currentMillis - ledMillis >= ledrate) {
+
+  // Switch LEDs back and forth.
+  // PUT IR IN D2, AND RED IN D3
+  if(currentMillis - ledMillis >= ledRate) {
     if(first_led == false) {
       digitalWrite(3,HIGH);
       digitalWrite(2,LOW);
@@ -79,5 +177,6 @@ void loop() {
     }
     ledMillis = currentMillis;
   }
-  
+
+  //End loop
 }
---
+++ readme.txt
@@ -0,0 +1,12 @@
+Uses an Arduino Nano V3, an Adafruit ADS1115, an OPT101, a BR1102W and an AN1102W IR and Red LED.
+
+Run this in with Arduino IDE or with the correct libraries in your preferred.
+I will get a CircuitPython version going.
+
+To use, just plug in the Nano, upload this script, and it should read out
+the ADS data. Uncomment the baseline, ratio, and score to see that. 
+The baseline resets when you get a bad signal so you can play around with that in case the scoring
+isn't working right.
+
+
+
